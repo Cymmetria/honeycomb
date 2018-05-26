25cdc66a3863c41bdc5420c769f4749ac8dff05a
Omer Cohen <git@omer.io>
2018-05-26 17:13:42 +0700

    integration docs

--- honeycomb/integrationmanager/integration_utils.py
+++ honeycomb/integrationmanager/integration_utils.py
@@ -12,32 +12,64 @@ logger = logging.getLogger(__name__)
 
 
 class BaseIntegration(object):
-    """Base Output Integration Class.
-
-    Will be overridden by output plugins.
-    """
+    """Base Output Integration Class."""
 
     __metaclass__ = ABCMeta
 
     def __init__(self, integration_data):
-        """__init__."""
+        """Use :func:`__init__` to set up any prerequisits needed before sending events, validate paramaters, etc.
+
+        :param integration_data: Integration parameters
+        :type integration_data: dict
+        :raises IntegrationMissingRequiredFieldError: If a required field is missing.
+        """
         self.integration_data = integration_data
         self.logger = logging.getLogger(__name__)
 
     @abstractmethod
-    def send_event(self, required_alert_fields):
-        """Send event."""
-        pass
+    def send_event(self, alert_dict):
+        """Send alert event to external integration.
+
+        :param alert_dict: A dictionary with all the alert fields.
+        :rtype: tuple(dict(output_data), object(output_file))
+        :raises IntegrationSendEventError: If there's a problem sending the event.
+        :raises IntegrationMissingRequiredFieldError: If a required field is missing.
+        :return: A tuple where the first value is a dictionary with information to display in the UI, and second is and
+                 optional file to be attahed. If polling is enabled, the returned output_data will be passed to
+                 :func:`poll_for_updates`. If your integration returns nothing, you should return ({}, None).
+        """
 
     @abstractmethod
     def format_output_data(self, output_data):
-        """format_output_data."""
-        pass
+        """Process and format the output_data returned by :func:`send_event` before display.
 
-    def test_connection(self, data):
-        """test_connection."""
+        This is currently only relevant for MazeRunner, if you don't return an output this should return output_data
+        without change.
+
+        :param output_data: As returned by :func:`send_event`
+        :rtype: dict
+        :return: MazeRunner compatible UI output.
+        :raises .IntegrationOutputFormatError: If there's a problem formatting the output data.
+        """
+
+    def test_connection(self, integration_data):
+        """Perform a test to ensure the integration is configured correctly.
+
+        This could include testing authentication or performing a test query.
+
+        :param integration_data: Integration arguments.
+        :returns: `success`
+        :rtype: tuple(bool(success), str(response))
+        """
         raise IntegrationNoMethodImplementationError()
 
     def poll_for_updates(self, integration_output_data):
-        """poll_for_updates."""
+        """Poll external service for updates.
+
+        If service has enabled polling, this method will be called periodically and should act like :func:`send_event`
+
+        :param integration_output_data: Output data returned by previous :func:`send_event` or :func:`poll_for_updates`
+        :return: See :func:`send_event`
+        :raises .IntegrationPollEventError: If there's a problem polling for updates.
+        """
         raise IntegrationNoMethodImplementationError()
